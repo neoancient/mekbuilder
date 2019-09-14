10fd23492081dac6eb1ce2b5a769563571410657
Carl Spain <neoancient@megamek.org>
2019-09-14 15:14:58 -0500

    Added BasicInfo tests.

--- javafx/src/test/kotlin/org/megamek/mekbuilder/javafx/view/BasicInfoTest.kt
+++ javafx/src/test/kotlin/org/megamek/mekbuilder/javafx/view/BasicInfoTest.kt
@@ -1,20 +1,20 @@
 package org.megamek.mekbuilder.javafx.view
 
-import com.sun.javafx.robot.FXRobot
-import javafx.embed.swing.JFXPanel
+import javafx.application.Platform
 import javafx.scene.Scene
 import javafx.scene.input.KeyCode
 import javafx.stage.Stage
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.Assertions.assertAll
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.function.Executable
 import org.megamek.mekbuilder.javafx.models.MekModel
+import org.megamek.mekbuilder.tech.TechBase
+import org.megamek.mekbuilder.tech.TechLevel
 import org.megamek.mekbuilder.unit.MekBuild
-import org.testfx.api.FxRobot
-import org.testfx.api.FxToolkit
 import org.testfx.framework.junit5.ApplicationTest
 import org.testfx.framework.junit5.Start
+import org.testfx.matcher.control.ComboBoxMatchers
 import tornadofx.*
 
 /**
@@ -22,7 +22,7 @@ import tornadofx.*
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class BasicInfoTest : ApplicationTest() {
-    lateinit var basicInfo: BasicInfo
+    private lateinit var basicInfo: BasicInfo
 
     @Start
     override fun start(stage: Stage) {
@@ -46,7 +46,7 @@ internal class BasicInfoTest : ApplicationTest() {
 
     @Test
     fun testSetChassis() {
-        basicInfo.model.chassisName = ""
+        basicInfo.txtChassis.text = ""
 
         clickOn(basicInfo.txtChassis)
         write("New Name")
@@ -63,12 +63,75 @@ internal class BasicInfoTest : ApplicationTest() {
 
     @Test
     fun testSetModelName() {
-        basicInfo.model.modelName = ""
+        basicInfo.txtModel.text = ""
 
         clickOn(basicInfo.txtModel)
         write("New Name")
 
         assertAll(Executable {assertEquals(basicInfo.txtModel.text, "New Name")},
                 Executable {assertEquals(basicInfo.txtModel.text, basicInfo.model.modelName)})
+    }
+
+    @Test
+    fun testInitialTechBaseValue() {
+        Platform.runLater {
+            basicInfo.model.techBase = TechBase.CLAN
+
+            assertThat(basicInfo.cbTechBase, ComboBoxMatchers.hasSelectedItem(basicInfo.model.techBase))
+        }
+    }
+
+    @Test
+    fun testSetTechBase() {
+        basicInfo.model.techBase = TechBase.IS
+
+        clickOn(basicInfo.cbTechBase)
+        type(KeyCode.DOWN)
+        type(KeyCode.DOWN)
+        type(KeyCode.ENTER)
+
+        assertAll(Executable {assertThat(basicInfo.cbTechBase, ComboBoxMatchers.hasSelectedItem(TechBase.ALL))},
+                Executable {assertEquals(basicInfo.model.techBase, TechBase.ALL)})
+    }
+
+    @Test
+    fun testSetTechLevel() {
+        basicInfo.cbTechLevel.selectionModel.select(TechLevel.STANDARD)
+
+        clickOn(basicInfo.cbTechLevel)
+        type(KeyCode.DOWN)
+        type(KeyCode.DOWN)
+        type(KeyCode.ENTER)
+
+        assertThat(basicInfo.cbTechLevel, ComboBoxMatchers.hasSelectedItem(TechLevel.EXPERIMENTAL))
+    }
+
+    @Test
+    fun testInitialSourceValue() {
+        basicInfo.model.source = "TRO: Awesome"
+        assertEquals(basicInfo.txtSource.text, basicInfo.model.source)
+    }
+
+    @Test
+    fun testSetSource() {
+        basicInfo.txtSource.text = ""
+
+        clickOn(basicInfo.txtSource)
+        write("TRO: 400 BC")
+
+        assertAll(Executable {assertEquals(basicInfo.txtSource.text, "TRO: 400 BC")},
+                Executable {assertEquals(basicInfo.txtSource.text, basicInfo.model.source)})
+    }
+
+    @Test
+    fun testMinimumYear() {
+        basicInfo.model.introYear = 3100
+
+        clickOn(basicInfo.txtYear)
+        eraseText(basicInfo.txtYear.length)
+        write("100")
+        type(KeyCode.TAB)
+
+        assertEquals(basicInfo.model.introYear, basicInfo.model.baseOption.introDate())
     }
 }
