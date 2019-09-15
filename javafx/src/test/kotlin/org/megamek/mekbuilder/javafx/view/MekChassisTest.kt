80c1af858a323b8557796ffd1f11cb527355def7
Carl Spain <neoancient@megamek.org>
2019-09-15 07:59:35 -0500

    Added license header to test classes.

--- javafx/src/test/kotlin/org/megamek/mekbuilder/javafx/view/MekChassisTest.kt
+++ javafx/src/test/kotlin/org/megamek/mekbuilder/javafx/view/MekChassisTest.kt
@@ -1,3 +1,21 @@
+/*
+ * MekBuilder - unit design companion of MegaMek
+ * Copyright (C) 2019 The MegaMek Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.megamek.mekbuilder.javafx.view
 
 import javafx.application.Platform
@@ -5,7 +23,6 @@ import javafx.scene.Scene
 import javafx.scene.control.SpinnerValueFactory
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyCodeCombination
-import javafx.scene.input.MouseButton
 import javafx.stage.Stage
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
--- javafx/src/test/kotlin/org/megamek/mekbuilder/javafx/view/MekConfigPanelTest.kt
+++ javafx/src/test/kotlin/org/megamek/mekbuilder/javafx/view/MekConfigPanelTest.kt
@@ -1,3 +1,21 @@
+/*
+ * MekBuilder - unit design companion of MegaMek
+ * Copyright (C) 2019 The MegaMek Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.megamek.mekbuilder.javafx.view
 
 import javafx.application.Platform
@@ -8,7 +26,6 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.function.Executable
 import org.megamek.mekbuilder.javafx.models.MekModel
 import org.megamek.mekbuilder.javafx.models.UnitViewModel
@@ -114,7 +131,7 @@ internal class MekConfigPanelTest : ApplicationTest() {
 
     @Test
     fun testSetOmni() {
-        model.omni = false;
+        model.omni = false
 
         clickOn(configPanel.chkOmni)
 
